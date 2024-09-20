c27a432a6d7d27b9be00b1846805508c1946e0f1
Rohit <rohit7067842611@gmail.com>
2024-09-20 11:17:07 +0530

    Initial commit

---
+++ src/sections/index.js
@@ -0,0 +1,11 @@
+import Hero from "./Hero";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+
+import CustomerReviews from "./CustomerReviews";
+
+export { Hero, Services, SpecialOffer, CustomerReviews, Footer, PopularProducts, SuperQuality, Subscribe }
